2b8deb76ff4dca61d4345642716363f25cbbb1c5
OlehVakulchyk <4020405@gmail.com>
2022-12-11 21:19:50 +0200

    ver 1.2.2
    working( install, entry point)

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -2,14 +2,17 @@ from setuptools import setup, find_namespace_packages
 
 
 setup(
-    name='clean_folder',
-    version='1.1.0',
+    name='clean_folder_olehV',
+    version='1.2.2',
     description='sorted folder',
     url='https://github.com/OlehVakulchyk/goit_hw_7',
     author='Oleh Vakulchyk',
     author_email='4020405@gmail.com',
     license='MIT',
     packages=find_namespace_packages(),
+    data_files=[('clean_folder', ['main.py', 'normalize.py',
+                 'subfunctions.py', 'start.py'])],
+    include_package_data=True,
         entry_points={'console_scripts': [
-        'clean-folder = clean_folder.main:start']}
+            'clean-folder = clean_folder.main:start']}
 )
